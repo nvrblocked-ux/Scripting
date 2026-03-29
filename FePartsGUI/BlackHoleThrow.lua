0b77cc533b28d16f2f94fa432d43cc877ca77eed
nvrblocked-ux <nvrblocked@gmail.com>
2026-03-28 22:55:50 -0500

    Add power slider and collect toggle to UI
    Added slider for power control and toggle for collecting unanchored parts.

--- FePartsGUI/BlackHoleThrow.lua
+++ FePartsGUI/BlackHoleThrow.lua
@@ -1,4 +1,5 @@
-local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+local WindUI = loadstring(game:HttpGet("https://github.com/Foo
+        agesus/WindUI/releases/latest/download/main.lua"))()
 
 local Window = WindUI:CreateWindow({
     Title = "Black Hole Thrower",
@@ -14,9 +15,38 @@ local Tab = Window:Tab({
 
 Tab:Select() -- Select Tab
 
+local Slider = Tab:Slider({
+    Title = "Power",
+    Desc = "Throw Power",
+    
+    -- To make float number supported, 
+    -- make the Step a float number.
+    -- example: Step = 0.1
+    Step = 1,
+    Value = {
+        Min = 20,
+        Max = 200,
+        Default = 20,
+    },
+    Callback = function(value)
+        print(value)
+    end
+})
+
+local Toggle = Tab:Toggle({
+    Title = "Collect",
+    Desc = "Collct Unanchored Parts",
+    Icon = "bird",
+    Type = "Checkbox",
+    Value = false, -- default value
+    Callback = function(state) 
+        print("Toggle Activated" .. tostring(state))
+    end
+})
+
 local Button = Tab:Button({
-    Title = "Button",
-    Desc = "Test Button",
+    Title = "Throw",
+    Desc = "Send it flying!"
     Locked = false,
     Callback = function()
         -- ...
